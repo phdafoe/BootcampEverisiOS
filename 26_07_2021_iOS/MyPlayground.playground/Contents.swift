b09f4c3067a66a0a7d749e13aeb77401f2850d07
phdafoe <phdafoe@gmail.com>
2021-07-26 08:28:38 +0200

    [WIP] - My first commit on Github -

---
+++ 26_07_2021_iOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// -> un comentario en una linea
+/*
+  -> Este es un comenatrioenvarias lineas
+ */
+
+// MARK: - TIPOS BASICOS
+
+var nombre: String = "pepe" //  var -> define una propiedad mutable
+let apellido: String = "ramirez" // let -> define una propiedad inmutable
+
+
+
+
+// MARK: - TIPOS BASICOS
+
